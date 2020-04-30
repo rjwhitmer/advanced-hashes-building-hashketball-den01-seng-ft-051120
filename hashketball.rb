bf1f18660596967db0a1e527e01cd406b8fe8fa5
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-30 00:54:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,10 +3,12 @@ def game_hash
   game_data = {
     :home => {
       :team_name => ""
+      :colors => ""
     },
     
     :away => {
       :team_name => ""
+      :colors => ""
     }
     
   }
