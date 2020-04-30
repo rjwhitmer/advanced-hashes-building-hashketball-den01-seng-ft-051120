71ccb92e7e60660b0287ec376224b9e825cf5dbc
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-30 01:25:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -60,8 +60,8 @@ def game_hash
     },
     
     :away => {
-      :team_name => "",
-      :colors => "",
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
       :players => [
         {:player_name => "",
           :assists => ,
