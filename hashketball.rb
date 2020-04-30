15960b00df0476757dc81d6f90407df5a530232d
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-30 01:34:09 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,124 @@
-# Write your code here!
+def game_hash
+  
+  game_data = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
+        {:player_name => "Alan Anderson",
+          :assists => 12,
+          :blocks => 1,
+          :number => 0,
+          :points => 22,
+          :rebounds => 12,
+          :shoe => 16,
+          :slam_dunks => 1,
+          :steals => 3
+        },
+        {:player_name => "Reggie Evans",
+          :assists => 12,
+          :blocks => 12,
+          :number => 30,
+          :points => 12,
+          :rebounds => 12,
+          :shoe => 14,
+          :slam_dunks => 7,
+          :steals => 12,
+        },
+        {:player_name => "Brook Lopez",
+          :assists => 10,
+          :blocks => 1,
+          :number => 11,
+          :points => 17,
+          :rebounds => 19,
+          :shoe => 17,
+          :slam_dunks => 15,
+          :steals => 3
+        },
+        {:player_name => "Mason Plumlee",
+          :assists => 6,
+          :blocks => 8,
+          :number => 1,
+          :points => 26,
+          :rebounds => 11,
+          :shoe => 19,
+          :slam_dunks => 5,
+          :steals => 3
+        },
+        {:player_name => "Jason Terry",
+          :assists => 2,
+          :blocks => 11,
+          :number => 31,
+          :points => 19,
+          :rebounds => 2,
+          :shoe => 15,
+          :slam_dunks => 1,
+          :steals => 4
+        },
+        
+      ]
+    },
+    
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => [
+        {:player_name => "Jeff Adrien",
+          :assists => 1,
+          :blocks => 7,
+          :number => 4,
+          :points => 10,
+          :rebounds => 1,
+          :shoe => 18,
+          :slam_dunks => 2,
+          :steals => 2
+        },
+        {:player_name => "Bismack Biyombo",
+          :assists => 7,
+          :blocks => 15,
+          :number => 0,
+          :points => 12,
+          :rebounds => 4,
+          :shoe => 16,
+          :slam_dunks => 10,
+          :steals => 22
+        },
+        {:player_name => "DeSagna Diop",
+          :assists => 12,
+          :blocks => 4,
+          :number => 2,
+          :points => 24,
+          :rebounds => 12,
+          :shoe => 14,
+          :slam_dunks => 5,
+          :steals => 5
+        },
+        {:player_name => "Ben Gordon",
+          :assists => 2,
+          :blocks => 1,
+          :number => 8,
+          :points => 33,
+          :rebounds => 3,
+          :shoe => 15,
+          :slam_dunks => 0,
+          :steals => 1
+        },
+        {:player_name => "Kemba Walker",
+          :assists => 12,
+          :blocks => 5,
+          :number => 33,
+          :points => 6,
+          :rebounds => 12,
+          :shoe => 15,
+          :slam_dunks => 12,
+          :steals => 7
+        },
+        
+      ]
+    }
+    
+  }
+  
+
+  game_data
+end
